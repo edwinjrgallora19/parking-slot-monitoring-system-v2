536b6a797210b37914927d478ce02f4383ace75c
edwinjrgallora19 <edwinjrgallora@gmail.com>
2026-05-19 23:46:41 +0800

    Connect frontend to deployed backend

--- frontend/parking-frontend/src/services/api.js
+++ frontend/parking-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://localhost:7035/api"
+    baseURL: "https://parking-slot-monitoring-api.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
